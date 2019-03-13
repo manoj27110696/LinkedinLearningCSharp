e623f204acae9a62587bfa3058a60c65ea6366a7
manoj k <manoj0134@gmail.com>
2019-03-12 22:40:28 -0400

    Change2

--- LinkedinLearningCSharp/Program.cs
+++ LinkedinLearningCSharp/Program.cs
@@ -7,7 +7,6 @@ namespace LinkedinLearningCSharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadKey();
         }
     }
 }
