e8cec4b7628153a9ec503fc0a04dea6c12b44cb4
manoj k <manoj0134@gmail.com>
2019-03-12 22:54:50 -0400

    using Variables-1

--- LinkedinLearningCSharp/Program.cs
+++ LinkedinLearningCSharp/Program.cs
@@ -6,7 +6,8 @@ namespace LinkedinLearningCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var a="MY String";
+            Console.WriteLine(a);
         }
     }
 }
