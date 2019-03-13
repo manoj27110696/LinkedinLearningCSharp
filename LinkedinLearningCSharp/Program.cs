197fdaa5e672661035be4e7ff17da3fe9638b8bf
manoj k <manoj0134@gmail.com>
2019-03-12 23:30:49 -0400

    Simple log on Console Application which checks for the Passcode

--- LinkedinLearningCSharp/Program.cs
+++ LinkedinLearningCSharp/Program.cs
@@ -6,29 +6,27 @@ namespace LinkedinLearningCSharp
     {
         static void Main(string[] args)
         {
-          Console.WriteLine("What is your name ?");
-          String a=  Console.ReadLine();
-          Console.WriteLine("How old are you ?");
-          String b=Console.ReadLine();
-          Console.WriteLine("What month were you born in");
-          String c=Console.ReadLine();
-          Console.WriteLine("Your Name : {0}",a);
-          Console.WriteLine("Your Age :{0}",b);
-          Console.WriteLine("You were born in the month : {0}",c);
-          if(c=="march")
+            var passcode="";
+            int nooftries=0;
+          while(passcode!="secret" && nooftries<3)
+          { 
+          Console.WriteLine("Enter the Passcode");
+          passcode=  Console.ReadLine();
+            if(passcode=="secret")
             {
-                Console.WriteLine("You are an Aries");
+                Console.WriteLine("Your are successfully authenticated");
             }
-          else  if (c == "april")
-            {
-                Console.WriteLine("You are a Taurus");
-            }
-          else  if (c == "July")
-            {
-                Console.WriteLine("You are an Orion");
-            }
-            Console.ReadKey();
+            else
+                { 
+                Console.WriteLine("please retry and enter the correct passcode");
+                nooftries++;
+                }
+          }
 
+          if(nooftries==3)
+            {
+                Console.WriteLine("You have exceeded the {0} attempts",nooftries);
+            }
 
         }
     }
