67ee58bebe92a4e6f95ef0569805b5a87f819477
manoj k <manoj0134@gmail.com>
2019-03-12 23:05:34 -0400

    Simple three question survey with Console Write and Console Read line

--- LinkedinLearningCSharp/Program.cs
+++ LinkedinLearningCSharp/Program.cs
@@ -6,8 +6,18 @@ namespace LinkedinLearningCSharp
     {
         static void Main(string[] args)
         {
-            var a="MY String";
-            Console.WriteLine(a);
+          Console.WriteLine("What is your name ?");
+          String a=  Console.ReadLine();
+          Console.WriteLine("How old are you ?");
+          String b=Console.ReadLine();
+          Console.WriteLine("What month were you born in");
+          String c=Console.ReadLine();
+          Console.WriteLine("Your Name : {0}",a);
+          Console.WriteLine("Your Age :{0}",b);
+          Console.WriteLine("You were born in the month : {0}",c);
+          Console.ReadKey();
+
+
         }
     }
 }
