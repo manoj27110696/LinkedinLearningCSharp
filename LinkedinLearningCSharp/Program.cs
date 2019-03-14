d90dd7f9bfda49fb65ff7ccbf3683b0f7fb457da
manoj k <manoj0134@gmail.com>
2019-03-14 00:16:15 -0400

    Loop from1 to 10 and then 10 to 1 . Repeat this 5 times using for loop

--- LinkedinLearningCSharp/Program.cs
+++ LinkedinLearningCSharp/Program.cs
@@ -6,10 +6,19 @@ namespace LinkedinLearningCSharp
     {
         static void Main(string[] args)
         {
-            for(int count=1;count<=10;count++)
+            for (int repeat = 1; repeat <= 5; repeat++)
             {
-                Console.WriteLine(count);
+                for (int count = 1; count <= 10; count++)
+                {
+                    Console.WriteLine(count);
+                }
+                for (int count = 10; count >= 1; count--)
+                {
+                    Console.WriteLine(count);
+                }
+
             }
+
         }
 
     }    
