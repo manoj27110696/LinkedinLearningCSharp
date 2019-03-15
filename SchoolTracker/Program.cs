91a1463782eb13a892f90014bb34e4e39391e26a
manoj k <manoj0134@gmail.com>
2019-03-14 23:30:23 -0400

    Created SchoolTracker used an array and foreach loop

---
+++ SchoolTracker/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchoolTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a program to accept to set of ten students
+             int[] StudentGrades=new int[]{80,77,45,87,54,42,67,24,86,98,40 };
+
+            foreach(var item in StudentGrades)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
